cb518692d8d2fc763f9e2595dcdd16272a309251
wjorellano <wilmanjunior2001@gmail.com>
2026-03-30 15:40:24 -0500

    Test: activando trigger manual

--- main.tf
+++ main.tf
@@ -5,23 +5,31 @@ provider "google" {
 
 data "google_project" "project" {}
 
+# PERMISO 1: Para que Cloud Build pueda desplegar en GKE
 resource "google_project_iam_member" "cloudbuild_gke_admin" {
   project = data.google_project.project.project_id
   role    = "roles/container.developer"
   member  = "serviceAccount:${data.google_project.project.number}@cloudbuild.gserviceaccount.com"
 }
 
+# PERMISO 2: Para que Cloud Build pueda leer/escribir en Artifact Registry
+resource "google_project_iam_member" "cloudbuild_registry" {
+  project = data.google_project.project.project_id
+  role    = "roles/artifactregistry.admin"
+  member  = "serviceAccount:${data.google_project.project.number}@cloudbuild.gserviceaccount.com"
+}
+
 resource "google_cloudbuild_trigger" "odoo_custom_trigger" {
   name        = "odoo-github-deploy"
-  description = "Despliegue automatico de modulos personalizados"
-  project     = "votos-app-99"
-  location    = "global"
+  description = "Despliegue automático de módulos personalizados"
+  location    = "us-central1" # Cámbialo si tu clúster está en otra región
 
-  # Cambiamos 'github' por 'trigger_template' si el anterior falla
-  # El nombre del repo lo sacas de la consola de GCP (Manage Repositories)
-  trigger_template {
-    branch_name = "master"
-    repo_name   = "github_wjorellano_odoo-custom-addons"
+  repository_event_config {
+    # Este es el ID de la conexión de 2da generación
+    repository = "projects/votos-app-99/locations/us-central1/connections/github-conn/repositories/odoo-custom-addons"
+    push {
+      branch = "^master$"
+    }
   }
 
   filename = "cloudbuild.yaml"
